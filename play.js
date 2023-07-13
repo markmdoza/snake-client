70ca8bdd02607f0dd63cd9384456a581bf5d2f75
Mark Mendoza <marklmdoza@gmail.com>
2023-07-12 22:56:20 -0400

    create a function called handleUserInput and register it as the data callback.

--- play.js
+++ play.js
@@ -1,14 +1,27 @@
 const myObject = require('./client');
 
-// setup interface to handle user input from stdin
 
 const setupInput = function () {
   const stdin = process.stdin;
+  
   stdin.setRawMode(true);
+  
   stdin.setEncoding("utf8");
+  
   stdin.resume();
+  
+  stdin.on("data", handleUserInput);
+  
+  
   return stdin;
 };
 
+// Create a function to handle user input
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+  process.exit();
+}
+  };
 
-myObject();
+  // Call setupInput and pass the handleUserInput function as an argument.
+  setupInput(handleUserInput);
