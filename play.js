133dbea7bc0d4abb24b04f8511a2c17d49501218
Mark Mendoza <marklmdoza@gmail.com>
2023-07-12 23:23:08 -0400

    export functions and update play.js

--- play.js
+++ play.js
@@ -1,27 +1,7 @@
-const myObject = require('./client');
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
+console.log('Connecting ... ');
+connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  
-  stdin.setRawMode(true);
-  
-  stdin.setEncoding("utf8");
-  
-  stdin.resume();
-  
-  stdin.on("data", handleUserInput);
-  
-  
-  return stdin;
-};
-
-// Create a function to handle user input
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-  process.exit();
-}
-  };
-
-  // Call setupInput and pass the handleUserInput function as an argument.
-  setupInput(handleUserInput);
+setupInput();
