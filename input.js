f564cbdea3b0af359b294cea9d1264e37ab888ee
Mark Mendoza <marklmdoza@gmail.com>
2023-07-13 16:24:34 -0400

    implement wasd movements

--- input.js
+++ input.js
@@ -1,4 +1,16 @@
-const setupInput = function () {
+// Create a new file called input.js [x]
+// Move handleUserInput and setupInput into the file. [x]
+// Export setupInput [x]
+// WASD Movement
+// In the input module, create a variable in the outer-most scope called connection, which can default to undefined. [x]
+// Update play.js to pass the object returned from connect into the setupInput function. []
+
+// Stores the active TCP connection object,
+let connection;
+
+const setupInput = (conn) => { // We changed setupInput to accept an object that lets us interact with the server. The connect() function you wrote earlier returns just such an object.
+  connection = conn;
+
   const stdin = process.stdin;
   
   stdin.setRawMode(true);
@@ -17,12 +29,18 @@ const setupInput = function () {
 const handleUserInput = function (key) {
   if (key === '\u0003') {
   process.exit();
+} else if (key === 'w') {
+  connection.write('Move: up\n');
+} else if (key === 'a') {
+  connection.write('Move: left\n');
+} else if (key === 's') {
+  connection.write('Move: down\n');
+} else if (key === 'd') {
+  connection.write('Move: right\n');
 }
   };
 
-  // Call setupInput and pass the handleUserInput function as an argument.
-  // setupInput(handleUserInput);
-
   module.exports = {
-    setupInput
+    setupInput,
+    connection
   };
