04f89f9949fdbd2802b5909e45b08027d88bf82c
Mark Mendoza <marklmdoza@gmail.com>
2023-07-12 22:03:57 -0400

    add the function setupInput to play.js

--- play.js
+++ play.js
@@ -1,3 +1,14 @@
 const myObject = require('./client');
 
-myObject;
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+
+myObject();
